e21aadd69459ee7131b27f624905a2d728ee550d
unknown <nagdatt.h.gajjam@gmail.com>
2022-08-26 07:03:51 +0530

    latest

--- src/components/FarmersPost/FarmersPosts.js
+++ src/components/FarmersPost/FarmersPosts.js
@@ -15,68 +15,105 @@ export default function FarmersPosts() {
         return { name, calories, fat, carbs, protein };
     }
 
-    const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-    ];
+
 
     const [data, setData] = React.useState([])
     useEffect(() => {
         axios.get("http://localhost:2000/users/getpost").then((res) => {
+            console.log(res.data, "uk")
             setData(res.data)
         }).catch((err) => {
             console.log(err)
         })
     }, [])
+    const handleBookNow = (orderId) => {
+        const id = sessionStorage.getItem("id")
+        const obj = {
+            id: orderId,
+            userId: id
+        }
+        console.log(obj, "obj")
+        axios.post("http://localhost:2000/users/book", obj).then((res) => {
+            console.log(res)
+            //  window.location.reload(false)
+        }).catch((err) => {
+            console.log(err)
+        })
+
+    }
+    const handleCollectNow = (orderId) => {
+
+        axios.get("http://localhost:2000/users/collect?id=" + orderId).then((res) => {
+            console.log(res)
+            window.location.reload(false)
+        }).catch((err) => {
+            console.log(err)
+        })
+
+    }
     return (
-        <div className="container" style={{margin:"20px"}}>
-            <table>
-                <th>Id</th>
-                <th>User Name</th>
-                <th>Location</th>
-                <th>Phone No.</th>
+        <div className="container" style={{ margin: "20px" }}>
 
-                <th>Book Now</th>
-                <th>Collect</th>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Raw material Id</TableCell>
+                            <TableCell>Quantity</TableCell>
+                            <TableCell align="left">User Name</TableCell>
+                            <TableCell align="left">Location</TableCell>
+                            <TableCell align="left">Phone No.</TableCell>
+                            <TableCell align="left">Book Now</TableCell>
+                            <TableCell align="v">Collect</TableCell>
 
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map((row) => (
+                            <TableRow
+                                key={row.rawmaterial}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row._id}
+                                </TableCell>
+                                <TableCell align="left">{row.qty}</TableCell>
 
-                {data.map((row) => {
-                    return (
-                        <tr>
-                            <td>{row._id}</td>
-                            <td>{row.farmerId.name}</td>
-                            <td>{row.location}</td>
-                            <td>{row.phoneNo}</td>
+                                <TableCell align="left">{"My Name"}</TableCell>
+                                <TableCell align="left">{row.location}</TableCell>
+                                <TableCell align="left">{row.phoneNo}</TableCell>
+                                <TableCell align="left">
+                                    <Button
+                                        type="button"
+                                        fullWidth
+                                        variant="contained"
+                                        sx={{ mt: 3, mb: 2 }}
+                                        onClick={() => handleBookNow(row._id)}
+                                        disabled={row.status >= 1 ? true : false}
+                                    >
+                                        Book Now
+                                    </Button>
+                                </TableCell>
+                                <TableCell align="left">
+                                    <Button
+                                        type="button"
+                                        fullWidth
+                                        variant="contained"
+                                        sx={{ mt: 3, mb: 2 }}
+                                        onClick={() => { handleCollectNow(row._id) }}
+                                        color={"secondary"}
+                                        disabled={row.status === 2 || row.status === 0 ? true : false}
 
-                            <td>
-                                <Button
-                                    type="button"
-                                    fullWidth
-                                    variant="contained"
-                                    sx={{ mt: 3, mb: 2 }}
-                                >
-                                    Book Now
-                                </Button>
-                            </td>
-                            <td>
-                                <Button
-                                    type="button"
-                                    fullWidth
-                                    variant="contained"
-                                    sx={{ mt: 3, mb: 2 }}
-                                    color={"secondary"}
-                                >
-                                   Collect
-                                </Button>
-                            </td>
-                        </tr>
-                    )
-                })
-                }
-            </table>
+                                    >
+                                        Collect
+                                    </Button>
+                                </TableCell>
+
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
 
     )
